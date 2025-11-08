da6dad34169276497a652f3f207ad888e4d53524
Lucas Carvalho <lucas_moc10@hotmail.com>
2025-11-08 09:53:36 -0300

    Removendo repositories

--- ClashOfClans.API/Repositories/IClanRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ClashOfClans.API.Model;
-
-namespace ClashOfClans.API.Repositories;
-
-public interface IClanRepository : IRepository<Clan>
-{
-    Task<Clan> ObterClanPorTag(string tag);
-    Task<bool> VerificarSeExisteClan(string tag);
-}
--- ClashOfClans.API/Repositories/IGuerraRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ClashOfClans.API.Model.Guerras;
-
-namespace ClashOfClans.API.Repositories;
-
-public interface IGuerraRepository : IRepository<Guerra>
-{
-    Task<Guerra> ObterGuerraPorDatas(DateTime inicioGuerra, DateTime fimGuerra);
-}
