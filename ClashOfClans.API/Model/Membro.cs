9e7943efc875b4b86db19c61ae1e48a240c67c18
Lucas Carvalho <lucas_moc10@hotmail.com>
2025-11-24 21:24:02 -0300

    Nova propriedade DataEntrada no modelo de Membro

--- ClashOfClans.API/Model/Membro.cs
+++ ClashOfClans.API/Model/Membro.cs
@@ -8,6 +8,7 @@ public class Membro(int clanId, string tag, string nome) : Entity
     public int ClanId { get; init; } = clanId;
     public string Tag { get; init; } = tag;
     public string Nome { get; private set; } = nome;
+    public DateTime DataEntrada { get; set; } = DateTime.Now;
     public SituacaoMembro Situacao { get; private set; } = SituacaoMembro.Ativo;
 
     public void AlterarNome(string nome)
