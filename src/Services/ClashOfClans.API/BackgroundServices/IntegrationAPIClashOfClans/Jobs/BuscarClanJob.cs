b393366659ad573ad1fa84bf25ffcc49d913045a
Lucas Carvalho <lucas_moc10@hotmail.com>
2026-01-15 21:02:48 -0300

    Movendo o de buscar clan para pasta específica

--- src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/BuscarClanJob.cs
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/BuscarClanJob.cs
@@ -71,23 +71,6 @@ public class BuscarClanJob(ClashOfClansService clashOfClansService, ClashOfClans
         await _mediator.Send(criarClan);
     }
 }
-public static class BuscarClanJobConfiguration
-{
-    public static void AddBuscarClanJob(this IServiceCollectionQuartzConfigurator configurator)
-    {
-        JobKey jobKey = new(nameof(BuscarClanJob));
-        configurator.AddJob<BuscarClanJob>(opts => opts.WithIdentity(jobKey));
-
-        configurator.AddTrigger(opts => opts
-            .ForJob(jobKey)
-            .WithIdentity($"{nameof(BuscarClanJob)}-trigger")
-            .StartNow()
-            .WithSimpleSchedule(x => x.WithIntervalInMinutes(5)
-            .RepeatForever())
-            );
-    }
-}
-
 public class ClanWarLeagueGroup
 {
     public string State { get; set; }
---
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/Configurations/BuscarClanJobConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace ClashOfClans.API.BackgroundServices.IntegrationAPIClashOfClans.Jobs.Configurations;
+
+public static class BuscarClanJobConfiguration
+{
+    public static void AddBuscarClanJob(this IServiceCollectionQuartzConfigurator configurator)
+    {
+        JobKey jobKey = new(nameof(BuscarClanJob));
+        configurator.AddJob<BuscarClanJob>(opts => opts.WithIdentity(jobKey));
+
+        configurator.AddTrigger(opts => opts
+            .ForJob(jobKey)
+            .WithIdentity($"{nameof(BuscarClanJob)}-trigger")
+            .StartNow()
+            .WithSimpleSchedule(x => x.WithIntervalInMinutes(5)
+            .RepeatForever())
+            );
+    }
+}
