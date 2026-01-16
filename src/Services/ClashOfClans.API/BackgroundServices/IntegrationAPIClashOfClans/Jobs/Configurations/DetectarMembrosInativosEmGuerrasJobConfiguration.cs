0f8182d01d249bbe57d0c547748de83dfc060694
Lucas Carvalho <lucas_moc10@hotmail.com>
2026-01-15 21:03:52 -0300

    Movendo o configuration de dectectar membros

---
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/Configurations/DetectarMembrosInativosEmGuerrasJobConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace ClashOfClans.API.BackgroundServices.IntegrationAPIClashOfClans.Jobs.Configurations;
+
+public static class DetectarMembrosInativosEmGuerrasJobConfiguration
+{
+    public static void AddDetectarMembrosInativosJob(this IServiceCollectionQuartzConfigurator configurator)
+    {
+        JobKey jobKey = new(nameof(DetectarMembrosInativosEmGuerrasJob));
+        configurator.AddJob<DetectarMembrosInativosEmGuerrasJob>(opts => opts.WithIdentity(jobKey));
+
+        configurator.AddTrigger(opts => opts
+            .ForJob(jobKey)
+            .WithIdentity($"{nameof(DetectarMembrosInativosEmGuerrasJob)}-trigger")
+            .StartNow()
+            .WithSimpleSchedule(x => x.WithIntervalInHours(2)
+            .RepeatForever())
+            );
+    }
+}
--- src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/DetectarMembrosInativosEmGuerras.cs
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/DetectarMembrosInativosEmGuerras.cs
@@ -80,19 +80,3 @@ public class DetectarMembrosInativosEmGuerrasJob(ClashOfClansContext context) : 
         }
     }
 }
-public static class DetectarMembrosInativosEmGuerrasJobConfiguration
-{
-    public static void AddDetectarMembrosInativosJob(this IServiceCollectionQuartzConfigurator configurator)
-    {
-        JobKey jobKey = new(nameof(DetectarMembrosInativosEmGuerrasJob));
-        configurator.AddJob<DetectarMembrosInativosEmGuerrasJob>(opts => opts.WithIdentity(jobKey));
-
-        configurator.AddTrigger(opts => opts
-            .ForJob(jobKey)
-            .WithIdentity($"{nameof(DetectarMembrosInativosEmGuerrasJob)}-trigger")
-            .StartNow()
-            .WithSimpleSchedule(x => x.WithIntervalInHours(2)
-            .RepeatForever())
-            );
-    }
-}
