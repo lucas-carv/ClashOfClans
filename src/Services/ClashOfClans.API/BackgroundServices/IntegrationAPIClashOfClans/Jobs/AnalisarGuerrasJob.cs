37c8d3d0d677dbc3a695f0e5d24f2560e324b484
Lucas Carvalho <lucas_moc10@hotmail.com>
2026-01-15 21:02:13 -0300

    Separando o Configuration de Guerras para pasta específica

--- src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/AnalisarGuerrasJob.cs
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/AnalisarGuerrasJob.cs
@@ -89,21 +89,3 @@ public class AnalisarGuerrasJob(ClashOfClansContext context, ILogger<AnalisarGue
         return membros;
     }
 }
-
-public static class AnalisarGuerrasJobConfiguration
-{
-    public static void AddAnalisarGuerras(this IServiceCollectionQuartzConfigurator configurator)
-    {
-        JobKey jobKey = new(nameof(AnalisarGuerrasJob));
-        configurator.AddJob<AnalisarGuerrasJob>(opts => opts.WithIdentity(jobKey));
-
-        configurator.AddTrigger(opts => opts
-            .ForJob(jobKey)
-            .WithIdentity($"{nameof(AnalisarGuerrasJob)}-trigger")
-            .StartNow()
-            //.StartAt(DateBuilder.FutureDate(1, IntervalUnit.Minute))
-            .WithSimpleSchedule(x => x.WithIntervalInMinutes(5)
-            .RepeatForever())
-            );
-    }
-}
