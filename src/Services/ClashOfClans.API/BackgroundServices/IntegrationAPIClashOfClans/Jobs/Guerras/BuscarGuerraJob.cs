4860981ff122585ac54a7159c49144e99da70651
Lucas Carvalho <lucas_moc10@hotmail.com>
2026-01-16 18:02:12 -0300

    Logando catch no job de buscar guerra

--- src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/Guerras/BuscarGuerraJob.cs
+++ src/Services/ClashOfClans.API/BackgroundServices/IntegrationAPIClashOfClans/Jobs/Guerras/BuscarGuerraJob.cs
@@ -15,81 +15,91 @@ public class BuscarGuerraJob(ClashOfClansService clashOfClansService, IMediator 
 
     public async Task Execute(IJobExecutionContext context)
     {
-        Console.WriteLine("Iniciando job de buscar guerra");
-        string tag = "#2L0UC9R8P";
-        string encodedTag = Uri.EscapeDataString(tag);
-
-        ResponseClashOfClans<WarResponse>? response = await _clashOfClansService.BuscarGuerra(encodedTag);
-        if (!response.IsValid)
+        try
         {
-            Console.WriteLine($"ERRO - Erro ao obter guerra {string.Join(",", response.Erros)}");
-            return;
-        }
 
-        WarResponse? war = response.ResponseData;
-        if (war is null)
-        {
-            Console.WriteLine("ERRO - Guerra não encontrada na api");
-            return;
-        }
+            Console.WriteLine("Iniciando job de buscar guerra");
+            string tag = "#2L0UC9R8P";
+            string encodedTag = Uri.EscapeDataString(tag);
 
-        if (war.State.Equals(StatusGuerra.NotInWar))
-            return;
-
-        ClanEmGuerraDTO clan = new()
-        {
-            ClanLevel = war.Clan.ClanLevel,
-            Nome = war.Clan.Name,
-            Tag = war.Clan.Tag,
-            Tipo = TipoClanGuerra.Principal,
-            Membros = war.Clan.Members.Select(m => new MembroEmGuerraDTO()
+            ResponseClashOfClans<WarResponse>? response = await _clashOfClansService.BuscarGuerra(encodedTag);
+            if (!response.IsValid)
             {
-                CentroVilaLevel = m.TownhallLevel,
-                Nome = m.Name,
-                Tag = m.Tag,
-                PosicaoMapa = m.MapPosition,
-                Ataques = m.Attacks.Select(a => new AtaquesDTO()
-                {
-                    Estrelas = a.Stars,
-                    AtacanteTag = a.AttackerTag,
-                    DefensorTag = a.DefenderTag,
-                    PercentualDestruicao = a.DestructionPercentage
-                })
-            })
-        };
+                Console.WriteLine($"ERRO - Erro ao obter guerra {string.Join(",", response.Erros)}");
+                return;
+            }
 
-        ClanEmGuerraDTO clanOponente = new()
-        {
-            ClanLevel = war.Opponent.ClanLevel,
-            Nome = war.Opponent.Name,
-            Tag = war.Opponent.Tag,
-            Tipo = TipoClanGuerra.Oponente,
-            Membros = war.Opponent.Members.Select(m => new MembroEmGuerraDTO()
+            WarResponse? war = response.ResponseData;
+            if (war is null)
             {
-                CentroVilaLevel = m.TownhallLevel,
-                Nome = m.Name,
-                Tag = m.Tag,
-                PosicaoMapa = m.MapPosition,
-                Ataques = m.Attacks.Select(a => new AtaquesDTO()
+                Console.WriteLine("ERRO - Guerra não encontrada na api");
+                return;
+            }
+
+            if (war.State.Equals(StatusGuerra.NotInWar))
+                return;
+
+            ClanEmGuerraDTO clan = new()
+            {
+                ClanLevel = war.Clan.ClanLevel,
+                Nome = war.Clan.Name,
+                Tag = war.Clan.Tag,
+                Tipo = TipoClanGuerra.Principal,
+                Membros = war.Clan.Members.Select(m => new MembroEmGuerraDTO()
                 {
-                    Estrelas = a.Stars,
-                    AtacanteTag = a.AttackerTag,
-                    DefensorTag = a.DefenderTag,
-                    PercentualDestruicao = a.DestructionPercentage
+                    CentroVilaLevel = m.TownhallLevel,
+                    Nome = m.Name,
+                    Tag = m.Tag,
+                    PosicaoMapa = m.MapPosition,
+                    Ataques = m.Attacks.Select(a => new AtaquesDTO()
+                    {
+                        Estrelas = a.Stars,
+                        AtacanteTag = a.AttackerTag,
+                        DefensorTag = a.DefenderTag,
+                        PercentualDestruicao = a.DestructionPercentage
+                    })
                 })
-            })
-        };
+            };
 
-        List<ClanEmGuerraDTO> clans = [clan, clanOponente];
+            ClanEmGuerraDTO clanOponente = new()
+            {
+                ClanLevel = war.Opponent.ClanLevel,
+                Nome = war.Opponent.Name,
+                Tag = war.Opponent.Tag,
+                Tipo = TipoClanGuerra.Oponente,
+                Membros = war.Opponent.Members.Select(m => new MembroEmGuerraDTO()
+                {
+                    CentroVilaLevel = m.TownhallLevel,
+                    Nome = m.Name,
+                    Tag = m.Tag,
+                    PosicaoMapa = m.MapPosition,
+                    Ataques = m.Attacks.Select(a => new AtaquesDTO()
+                    {
+                        Estrelas = a.Stars,
+                        AtacanteTag = a.AttackerTag,
+                        DefensorTag = a.DefenderTag,
+                        PercentualDestruicao = a.DestructionPercentage
+                    })
+                })
+            };
 
-        UpsertGuerraRequest upsertGuerraRequest = new(
-            war.State.ToString(),
-            war.StartTime,
-            war.EndTime,
-            "Normal",
-            clans);
+            List<ClanEmGuerraDTO> clans = [clan, clanOponente];
 
-        await _mediator.Send(upsertGuerraRequest);
-        Console.WriteLine("Finalizando job de buscar guerra");
+            UpsertGuerraRequest upsertGuerraRequest = new(
+                war.State.ToString(),
+                war.StartTime,
+                war.EndTime,
+                "Normal",
+                clans);
+
+            await _mediator.Send(upsertGuerraRequest);
+            Console.WriteLine("Finalizando job de buscar guerra");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERRO ao buscar guerra {ex.Message} \n{ex.InnerException} ");
+            throw;
+        }
     }
 }
